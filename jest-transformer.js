60f1143145e5c1eb993e99f928e1c37dc4afc4b4
PanyushkinD <PanyushkinD@quantumart.ru>
2017-02-23 09:47:59 +0300

    Separate tsconfig for tests

--- jest-transformer.js
+++ jest-transformer.js
@@ -1,15 +1,15 @@
 var ts = require("typescript");
 var fs = require("fs");
 
+var tsconfig = JSON.parse(fs.readFileSync("./src/__tests__/tsconfig.json", "utf8"));
+
+tsconfig.target = ts.ScriptTarget.ES5;
+
 module.exports = {
-    process: function(src, filename) {
+    process: function (src, filename) {
         if (filename.indexOf("node_modules") === -1) {
             if (filename.indexOf(".ts") === filename.length - 3) {
-                src = ts.transpile(src, {
-                    experimentalDecorators: true,
-                    lib: [ "dom", "es5", "es2015.symbol" ],
-                    target: ts.ScriptTarget.ES5,
-                });
+                src = ts.transpile(src, tsconfig);
             }
         }
         return src;
